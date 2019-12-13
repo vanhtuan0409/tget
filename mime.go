248bf7e7ba4f6966448309c56ba28b8fbc7427f9
Tuan Vuong <vanhtuan0409@gmail.com>
2019-12-13 18:31:19 +0700

    update checksum error

--- mime.go
+++ mime.go
@@ -11,18 +11,17 @@ import (
 
 func getDownloadedPath(target string, resp *http.Response) string {
 	st, err := os.Stat(target)
-	if err == nil && !st.IsDir() {
-		return target
+	if err == nil && st.IsDir() {
+		contentDisposition := resp.Header.Get(contentDispositionHeader)
+		_, params, err := mime.ParseMediaType(contentDisposition)
+		if err != nil {
+			return path.Join(target, fmt.Sprintf("tget-file-%d", time.Now().Unix()))
+		}
+		fName, ok := params["filename"]
+		if !ok {
+			return path.Join(target, fmt.Sprintf("tget-file-%d", time.Now().Unix()))
+		}
+		return path.Join(target, fName)
 	}
-
-	contentDisposition := resp.Header.Get(contentDispositionHeader)
-	_, params, err := mime.ParseMediaType(contentDisposition)
-	if err != nil {
-		return path.Join(target, fmt.Sprintf("tget-file-%d", time.Now().Unix()))
-	}
-	fName, ok := params["filename"]
-	if !ok {
-		return path.Join(target, fmt.Sprintf("tget-file-%d", time.Now().Unix()))
-	}
-	return path.Join(target, fName)
+	return target
 }
